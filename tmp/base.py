401bdcee7195e2b57efa908efa0516f1564f4389
xen-io <undergroundsaveliy@gmail.com>
2023-09-25 17:52:38 +0500

    temp file added

---
+++ tmp/base.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import sqlalchemy as sa
+
+engine = sa.create_engine('sqlite://')
+
+meta = sa.MetaData()
+
+users = sa.Table('users', meta,
+    sa.Column('id', sa.Integer, 
+        primary_key=True, nullable=False, 
+        unique=True, index=True),
+    sa.Column('full_name', sa.String),
+    sa.Column('email', sa.String),
+    sa.Column('password', sa.String),
+    sa.Column('info', sa.String),
+    sa.Column('created_projects', sa.String)
+)
+
+projects = sa.Table('projects', meta, 
+    sa.Column('id', sa.Integer,
+        primary_key=True, nullable=False, 
+        unique=True, index=True),
+    sa.Column('name', sa.String),
+    sa.Column('creator', sa.ForeignKey('users.id')),
+    sa.Column('status', sa.String),
+    sa.Column('info', sa.String),
+    sa.Column('date_planned', sa.DateTime),
+    sa.Column('date_actual', sa.DateTime)
+)
+
+members = sa.Table('editors', meta,
+    sa.Column('type', sa.String),
+    sa.Column('proj_id', sa.ForeignKey('projects.id')),
+    sa.Column('user_id', sa.ForeignKey('users.id'))
+)
+
+nodes = sa.Table('node', meta,
+    sa.Column('id', sa.Integer,
+        primary_key=True, nullable=False, 
+        unique=True, index=True),
+    sa.Column('proj_id', sa.ForeignKey('projects.id')),
+    sa.Column('type', sa.String),
+    sa.Column('status', sa.String),
+    sa.Column('info', sa.String),
+    sa.Column('date_planned', sa.DateTime),
+    sa.Column('date_actual', sa.DateTime)
+)
+
+dependencies = sa.Table('dependencies', meta,
+    sa.Column('id', sa.Integer,
+        primary_key=True, nullable=False, 
+        unique=True, index=True),
+    sa.Column('proj_id', sa.ForeignKey('projects.id')),
+    sa.Column('from', sa.ForeignKey('nodes.id')),
+    sa.Column('to', sa.ForeignKey('nodes.id'))
+)
+
+cookies = sa.Table('cookies', meta,
+    sa.Column('cookie', sa.Integer,
+        primary_key=True, nullable=False, 
+        unique=True, index=False),
+    sa.Column('user_id', sa.ForeignKey('users.id'))
+)
+
+cookies = sa.Table('cookies', meta,
+    sa.Column('cookie', sa.Integer,
+        primary_key=True, nullable=False, 
+        unique=True, index=False),
+    sa.Column('user_id', sa.ForeignKey('users.id'))
+)
